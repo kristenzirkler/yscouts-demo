78ad647a8c128db188a11959a23e79e73f272e84
Kristen Zirkler <bazirkler@gmail.com>
2016-03-01 10:04:53 -0800

    new footer

--- wp-files/sidebar-footer.php
+++ wp-files/sidebar-footer.php
@@ -7,29 +7,6 @@
 
 // count the active sidebars to determine column sizes
 $sidebars = is_active_sidebar( 'first-footer-widget-area' ) + is_active_sidebar( 'second-footer-widget-area' ) + is_active_sidebar( 'third-footer-widget-area' ) + is_active_sidebar( 'fourth-footer-widget-area' );
-$sidebargrid = "grid-25";
-
-if ($sidebars == "1") {
-	
-	// if only one - fullwidth
-	$sidebargrid = "grid-100 tablet-grid-100 mobile-grid-100";
-
-} elseif ($sidebars == "2") {
-	
-	// if two, split in half
-	$sidebargrid = "grid-50 tablet-grid-50 mobile-grid-100";
-	
-} elseif ($sidebars == "3") {
-	
-	// if three, divide in thirds
-	$sidebargrid = "grid-33 tablet-grid-33 mobile-grid-100";
-
-} elseif ($sidebars == "4") {
-	
-	// if four, split in fourths
-	$sidebargrid = "grid-25 tablet-grid-50 mobile-grid-100";
-	
-} ?>
 
 <?php if ($sidebars) : ?>
     
@@ -39,17 +16,17 @@ if ($sidebars == "1") {
             
         <?php if (is_active_sidebar( 'first-footer-widget-area' )) : ?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="first-footer-widget-area">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
             </ul>
         </div>
         
         <?php endif;?>
-        
+
         <?php if (is_active_sidebar( 'second-footer-widget-area' )) :?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="second-footer-widget-area">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
             </ul>
@@ -61,7 +38,7 @@ if ($sidebars == "1") {
         
         <?php if (is_active_sidebar( 'third-footer-widget-area' )) : ?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="third-footer-widget-area">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
             </ul>
@@ -71,7 +48,7 @@ if ($sidebars == "1") {
         
         <?php if (is_active_sidebar( 'fourth-footer-widget-area' )) : ?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="fourth-footer-widget-area">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
             </ul>
