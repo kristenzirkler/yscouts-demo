65c2a7bc0357c58e82fa6be483f324867371b1ad
Kristen Zirkler <bazirkler@gmail.com>
2016-03-30 13:02:26 -0700

    Final Changes

--- js/custom-wp-js.js
+++ js/custom-wp-js.js
@@ -1,2 +1,16 @@
 // Main JS File Unminified 
-jQuery(function(){function updateWord(){jQuery('#home-word-change').text('Leadership');}setTimeout(updateWord, 2000);});
+jQuery(function(){
+	 // Video Modal
+  jQuery('[rel="video-link"]').click(function(event) {
+    event.preventDefault();
+    var videoID = $(this).attr('data-videoid');
+    jQuery('#video-modal').show();
+    jQuery('#video-modal iframe').attr('src','//www.youtube.com/embed/'+videoID+'?autoplay=1&autohide=1&modestbranding=1&rel=0&showinfo=0&wmode=transparent');
+    jQuery('body').css('overflow','hidden').css('position','relative');
+  });
+  jQuery('[rel="video-modal-close"]').click(function(event) {
+    jQuery('#video-modal').hide();
+    jQuery('#video-modal iframe').attr('src','');
+    jQuery('body').css('overflow','visible').css('position','static');
+  });
+});
--- wp-files/sidebar-footer.php
+++ wp-files/sidebar-footer.php
@@ -7,49 +7,52 @@
 
 // count the active sidebars to determine column sizes
 $sidebars = is_active_sidebar( 'first-footer-widget-area' ) + is_active_sidebar( 'second-footer-widget-area' ) + is_active_sidebar( 'third-footer-widget-area' ) + is_active_sidebar( 'fourth-footer-widget-area' );
-$sidebargrid = "grid-25";
+$sidebargrid = "grid-100";
 
 if ($sidebars == "1") {
     
     // if only one - fullwidth
-    $sidebargrid = "grid-100 tablet-grid-100 mobile-grid-100";
+    //$sidebargrid = "grid-100 tablet-grid-100 mobile-grid-100";
+    $sidebargrid = "grid-100";
 
 } elseif ($sidebars == "2") {
     
     // if two, split in half
-    $sidebargrid = "grid-50 tablet-grid-50 mobile-grid-100";
+    //$sidebargrid = "grid-50 tablet-grid-50 mobile-grid-100";
+    $sidebargrid = "grid-100";
     
 } elseif ($sidebars == "3") {
     
     // if three, divide in thirds
-    $sidebargrid = "grid-33 tablet-grid-33 mobile-grid-100";
+    //$sidebargrid = "grid-33 tablet-grid-33 mobile-grid-100";
+    $sidebargrid = "grid-100";
 
 } elseif ($sidebars == "4") {
     
     // if four, split in fourths
-    $sidebargrid = "grid-25 tablet-grid-50 mobile-grid-100";
+    //$sidebargrid = "grid-25 tablet-grid-50 mobile-grid-100";
+    $sidebargrid = "grid-100";
     
 } ?>
 
 <?php if ($sidebars) : ?>
     
-    <div class="ut-footer-area">
-     
-        <div class="grid-container">
-            
-        <?php if (is_active_sidebar( 'first-footer-widget-area' )) : ?>
-        
+    <?php if (is_active_sidebar( 'first-footer-widget-area' )) : ?>
+    <div class="ys-footer-contact first-footer-widget-area">
         <div class="<?php echo $sidebargrid;?>">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
             </ul>
         </div>
-        
-        <?php endif;?>
+    </div>
+    <?php endif;?>
+
+    <div class="ut-footer-area">
+
         
         <?php if (is_active_sidebar( 'second-footer-widget-area' )) :?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="second-footer-widget-area <?php echo $sidebargrid;?>">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
             </ul>
@@ -61,7 +64,7 @@ if ($sidebars == "1") {
         
         <?php if (is_active_sidebar( 'third-footer-widget-area' )) : ?>
         
-        <div class="<?php echo $sidebargrid;?>">
+        <div class="third-footer-widget-area <?php echo $sidebargrid;?>">
             <ul class="sidebar">
                 <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
             </ul>
@@ -78,8 +81,6 @@ if ($sidebars == "1") {
         </div>
         
         <?php endif;?>
-        
-        </div>
     
     </div>
     
